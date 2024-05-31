bce53eecb38571d1b5da38721bc0c9f4dfeea14d
Janarthanan2107 <131163725+Janarthanan2107@users.noreply.github.com>
2024-05-31 09:47:59 +0530

    Update NotFound.jsx

--- src/routes/NotFound/NotFound.jsx
+++ src/routes/NotFound/NotFound.jsx
@@ -1,9 +1,11 @@
+import logo from "../../assets/images/logo/logo.png";
+
 const NotFound = () => {
   return (
     <div>
       <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
         <div className="flex flex-col justify-center items-center">
-          <img src="src\assets\images\logo\logo.png" alt="Logo"></img>
+          <img src={logo} alt="Logo"></img>
           <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
             Page not found
           </h1>
